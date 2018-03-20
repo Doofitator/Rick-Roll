ea8b99c956f2e6153fc2182858d280f59e1ed87d
Doofitator <ashley@sharkey.id.au>
2018-03-20 18:43:55 +1100

    Initial Commit

---
+++ ConsoleApp1/Form1.vb
@@ -0,0 +1,18 @@
+﻿Imports System.Runtime.InteropServices
+Public Class Form1
+    <DllImport("user32.dll", SetLastError:=True, CharSet:=CharSet.Auto)> Private Shared Function SendMessage(ByVal hWnd As IntPtr, ByVal Msg As UInteger, ByVal wParam As IntPtr, ByVal lParam As IntPtr) As IntPtr
+    End Function
+    Const WM_APPCOMMAND As UInteger = &H319
+    Const APPCOMMAND_VOLUME_UP As UInteger = &HA
+    Const APPCOMMAND_VOLUME_DOWN As UInteger = &H9
+    Const APPCOMMAND_VOLUME_MUTE As UInteger = &H8
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim i As Integer
+
+        While i < 100
+            SendMessage(Me.Handle, WM_APPCOMMAND, &H30292, APPCOMMAND_VOLUME_UP * &H10000)
+            i += 1
+        End While
+    End Sub
+End Class
---
+++ ConsoleApp1/Module1.vb
@@ -0,0 +1,43 @@
+﻿
+
+Module Module1
+    Private Declare Function SystemParametersInfo Lib “user32” Alias “SystemParametersInfoA” (ByVal uAction As Integer, ByVal uParam As Integer, ByVal lpvParam As String, ByVal fuWinIni As Integer) As Integer
+
+    Private Declare Auto Function ShowWindow Lib "user32.dll" (ByVal hWnd As IntPtr, ByVal nCmdShow As Integer) As Boolean
+    Private Declare Auto Function GetConsoleWindow Lib "kernel32.dll" () As IntPtr
+    Private Const SW_HIDE As Integer = 0
+
+    'Declare two constant
+
+    Private Const SETDESKWALLPAPER = 20
+
+    Private Const UPDATEINIFILE = &H1
+
+
+
+    Sub Main()
+        Dim hWndConsole As IntPtr
+        hWndConsole = GetConsoleWindow()
+        ShowWindow(hWndConsole, SW_HIDE)
+
+        Dim webClient As New System.Net.WebClient
+
+        webClient.DownloadFile("https://i.ytimg.com/vi/dQw4w9WgXcQ/maxresdefault.jpg", "C:\Users\" + Environment.UserName + "\rick.jpg")
+
+
+        SystemParametersInfo(SETDESKWALLPAPER, 0, "C:\Users\" + Environment.UserName + "\rick.jpg", UPDATEINIFILE)
+
+        MsgBox("You got Rick Rolled.", vbInformation, "Check your desktop background!")
+
+        Dim form As New Form1()
+        form.Show()
+
+        My.Computer.Audio.Play("\\SERVER\share\roll.wav")
+        System.Threading.Thread.Sleep(200000)
+
+    End Sub
+
+
+
+
+End Module
